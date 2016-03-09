864016e14c1caa3ddf42e25cd34849969a21eff7
Chris Hitchcott <hitchcott@gmail.com>
2016-03-09 17:07:27 +0000

    add mining script

---
+++ scripts/blockchain/start-blockchain.sh
@@ -0,0 +1,38 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ACCOUNT_PASSWORD="$(cat "${DIR}/password")"
+
+# create start scripts
+baseCommand="geth \
+--datadir=${DIR}/tmp/ --logfile=${DIR}/tmp/blockchain.log \
+--rpc --rpcaddr localhost --rpccorsdomain \"*\" \
+--networkid 58554 \
+--genesis=${DIR}/genesis.json \
+--maxpeers 1 --password ${DIR}/password"
+# append account details
+accountList="${baseCommand} account list"
+accountNew="${baseCommand} account new"
+
+# create temp directory and empty log file
+mkdir -p "${DIR}/tmp/" && touch "${DIR}/tmp/blockchain.log"
+
+# check if we have any accounts
+if [[ "$(${accountList})" =~ {(.*)} ]]; then
+  ETH_ACCOUNT="${BASH_REMATCH[1]}"
+else
+  echo "No Accounts found, creating a new one"
+  $accountNew
+  # try agian
+  if [[ "$(${accountList})" =~ {(.*)} ]]; then
+    ETH_ACCOUNT="${BASH_REMATCH[1]}"
+  else
+    echo "Failed creating account"
+    exit
+  fi
+fi
+
+# create minin command
+miningScript="${baseCommand} --unlock ${ETH_ACCOUNT} js ${DIR}/miner.js"
+echo $miningScript
+
+# start the mining script
+$miningScript
